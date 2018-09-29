629c96869ba367171d2556c4e240e033e2e177c3
Ilya <xwqtrdk@gmail.com>
2018-09-29 15:52:24 +0300

    Insert Buttons Added

--- WYSIWYG.js
+++ WYSIWYG.js
@@ -1,60 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function () {
   PopUpHide();
 });
 var editArea = $("#htmlEditArea");
 var showArea = $("#EditorView");
 var InsertUrlBtn = $("#InsertUrlBtn");
-var InsertImageBtn= $("#InsertImageBtn");
+var InsertImageBtn = $("#InsertImageBtn");
+var InsertButtonBtn = $("#InsertButtonBtn");
+var InsertListBtn = $("#InsertListBtn");
+
 var popup = $("#Popup");
 var popupText = $("#PopupText")
-var popupSave =$("#PopupSaveBtn");
+var popupSave = $("#PopupSaveBtn");
 var popupCancelBtn = $("#PopupCancelBtn");
-var popupEdit =$("#PopupEdit");
-
-
-popupCancelBtn.on("click",(e)=>{
+var popupBody = $("#PopupBody");
+popupCancelBtn.on("click", (e) => {
   PopUpHide();
 })
-showArea.RefreshView = (data="") => {
-  debugger;
-  showArea.empty();
-  editArea.val(editArea.val()+data);
-  showArea.append(editArea.val());
-  
-  
-}
 
-InsertUrlBtn.on('click',(e)=> {
-  PopUpShow("Add Url",()=>{
-    debugger;
-    let x = "<a href='"+ popupEdit.val()+"'>"+popupEdit.val()+"</a>";
-    
-    showArea.RefreshView(x);
-    PopUpHide(); 
-  });
-})
-InsertImageBtn.on('click',(e)=> {
-  PopUpShow("Add Image",()=>{
-    debugger;
-    let x = "<img src='"+ popupEdit.val()+"'></img>";
-    showArea.RefreshView(x);
-    
-    PopUpHide(); 
-  });
-})
-function PopUpShow(description,saveAction) {
-  popup.show();
-  debugger;
+function PopUpShow(description, body, saveAction) {
+  popupBody.empty();
+  popupBody.append(body);
   popupText.empty();
   popupText.append(description);
   popupSaveAction = saveAction;
+  popup.show();
 }
+
 function PopUpHide() {
   popup.hide();
 }
-var popupSaveAction =null;
-popupSave.on('click',()=> popupSaveAction());
+var popupSaveAction = null;
+popupSave.on('click', () => popupSaveAction());
 
+
+
+
+
+showArea.RefreshView = (data = "") => {
+  showArea.empty();
+  editArea.val(editArea.val() + data);
+  showArea.append(editArea.val());
+}
 editArea.keyup((e) => {
   debugger;
   showArea.RefreshView();
@@ -69,7 +55,7 @@ editArea.on('paste', (event) => {
       if (item.type.includes('image')) {
         reader.onload = (e) => {
           itemsCnt++;
-          var imagetag = "<img src='"+e.target.result+"'/>";
+          var imagetag = "<img src='" + e.target.result + "'/>";
           event.currentTarget.value += imagetag;
 
         };
@@ -79,4 +65,49 @@ editArea.on('paste', (event) => {
     }
   }
 
+})
+
+InsertListBtn.on('click',(e)=> {
+  var pBody = "<textarea id='btnListText'>Split items using enter</textarea>";
+  
+  PopUpShow("Add List",pBody,()=>{ 
+    var collection = $("#btnListText").val().split("\n");
+    let x = "<ul>";
+    for(let text in collection)
+    {
+      x+='<li>'+text+'</li>';
+    }
+    x+="</ul>"
+    showArea.RefreshView(x);
+    PopUpHide();
+  });
+ 
+})
+
+InsertButtonBtn.on('click',(e)=> {
+  let pBody = "<textarea id='btnText'>Enter Btn Text</textarea>";
+  pBody+="<textarea id='btnAction'>Enter Btn Action</textarea>";
+  PopUpShow("Add Button",pBody,()=>{ 
+    let x = "<button onclick='"+$('#btnAction').val()+"'>"+$('#btnText').val() +"</button>";
+    showArea.RefreshView(x);
+    PopUpHide();
+  });
+})
+
+InsertUrlBtn.on('click', (e) => {
+  let pBody = "<textarea id='enterUrl'>Enter Uri</textarea>";
+  pBody += "<textarea id='enterText'>Enter Uri text</textarea>"
+  PopUpShow("Add Url", pBody, () => {
+    let x = "<a href='" + $("#enterUrl").val() + "'>" + $("#enterText").val() + "</a>";
+    showArea.RefreshView(x);
+    PopUpHide();
+  });
+})
+InsertImageBtn.on('click', (e) => {
+  let pBody = "<textarea id='enterUrl'>Enter Uri</textarea>";
+  PopUpShow("Add Image", pBody, () => {
+    let x = "<img src='" + $("#enterUrl").val() + "'></img>";
+    showArea.RefreshView(x);
+    PopUpHide();
+  });
 })
