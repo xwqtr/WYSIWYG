1034d6e8af15b1ae357de4a82c660dc6476f49f7
Ilya Sekachev (Akvelon INC) <v-ilseka@microsoft.com>
2018-10-31 13:42:16 +0300

    Save page as zip

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const Archiver = require('archiver');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/src'));
+
 app.get('/',(req,res)=> {
     res.set('Content-Type', 'text/html');
     var options = {
@@ -22,6 +32,28 @@ app.get('/',(req,res)=> {
       });
     
 });
+
+app.post('/download-zip-file', function(req, res){ 
+
+
+  var archive = Archiver('zip');
+  archive.on('error', function(err) {
+      res.status(500).send({error: err.message});
+  });
+
+  res.on('close', function() {
+      console.log('Archive wrote %d bytes', archive.pointer());
+      return res.status(200).send('OK').end();
+  });
+  res.attachment('page.zip');
+  archive.pipe(res);
+  archive.append(req.body.pageHtml, {name:'page.html'});
+  archive.finalize();
+});
+
+
+
+
 const port =process.env.PORT||3000;
 app.listen(port,()=>console.log('Listening on port ' + port));
 
